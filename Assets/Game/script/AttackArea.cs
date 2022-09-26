6e07d50bb50ee460f0bcb604cd2884325c6a88bb
minhnguyen21190 <minh.gaiatech@gmail.com>
2022-09-26 18:47:43 +0700

    +AttackArea

--- Assets/Game/script/AttackArea.cs
+++ Assets/Game/script/AttackArea.cs
@@ -6,7 +6,7 @@ public class AttackArea : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player" || collision.tag == "Enemy")
+        if (collision.tag == "Player" || collision.tag == "Enemy")// neu va cham Player hoac Enemy thi se giam damge
         {
             collision.GetComponent<Character>().OnHit(30f);
         }
